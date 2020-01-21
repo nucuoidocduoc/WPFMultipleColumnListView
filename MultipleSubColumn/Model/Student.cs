9796a7ca8a67a13c5eef5b995c73fa5d4886f84a
phuong_nt <phuong_nt@anybim.vn>
2020-01-21 10:00:42 +0700

    init

---
+++ MultipleSubColumn/Model/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleSubColumn.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public Info Info { get; set; }
+    }
+}
---
+++ MultipleSubColumn/ViewModel.cs
@@ -0,0 +1,36 @@
+﻿using MultipleSubColumn.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleSubColumn
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Student> _students;
+        public ObservableCollection<Student> Students { get => _students; set => SetPropertyChange(ref _students, value); }
+
+        public ViewModel()
+        {
+            _students = new ObservableCollection<Student>() {
+                new Student(){Id=1,Info=new Info(){FirstName="Nguyen",MiddleName="Trong",LastName="Dao"}
+            }};
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void SetPropertyChange<T>(ref T member, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(member, value)) {
+                return;
+            }
+            member = value;
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
